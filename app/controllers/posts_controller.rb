b459e47c84ca104a66eace5ed2c09447c074d50b
mymadhead <mymadhead@gmail.com>
2020-07-04 19:47:48 +0300

    add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :preview, :body)
+  end
 end
